8a472a2ccfb910a44b759978b11fd0121ff9c04f
Max Egger <maestro@Maxs-MacBook-Pro.local>
2019-03-13 17:06:51 +0100

    foundation folder

--- color/Scripts/color_checker.py
+++ color/Scripts/color_checker.py
@@ -15,12 +15,12 @@ form = cgi.FieldStorage()
 # get an input filed from the form called 'user_color'
 # and assign it's value to a local variable called user_color
 user_color = form.getvalue('color')
-    
+
 with open('colors.csv') as _filehandler:
     csv_file_reader = csv.reader(_filehandler)
     check = False
     for row in csv_file_reader:
-        if user_color.title()== row[1]:
+        if user_color.title() == row[1]:
         	check = True
         	break
     if check == True:
@@ -28,22 +28,51 @@ with open('colors.csv') as _filehandler:
     		<!DOCTYPE html>
 			<html>
 			<head>
-			<title> Server Answer </title>
+			<style>
+			p {
+				font-family: Courier New;
+				font-size: 30px;
+				color: %s;
+				filter: invert(1);
+			}
+			#big{
+				font-size: 120px;
+			}
+			</style>
+			<title> Hey Adam </title>
 			</head>
-			<body>
+			<body style="background-color:%s">
+			<br><br><br><br><br><br><br><br><br><br><br><br>
+			<center>
 			<p> %s is a valid color! The Hexcode is: %s </p>
+			<p> The color of this text is the complementary of the Backgroundcolor.
+			<p>#WhenYourCodeGameIsStrong!</p>
+			<br><p> what do you think Adam?</p>
+			</center>
 			</body>
 			</html>
-    		"""%(user_color ,  row[2]))
+    		"""%(row[2], row[2], user_color, row[2]))
     if check == False:
     	print("""
     		<!DOCTYPE html>
 			<html>
 			<head>
+			<style>
+			div.container {
+  				border: 2px solid black;
+  				width: 200px;
+			}
+			</style>
 			<title> Server Answer </title>
 			</head>
 			<body>
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<center>
+			<div class="container">
 			<p> %s is not a valid color.</p>
+			<p id="big"> Sorry, bro</p>
+			</div>
+			</center>
 			</body>
 			</html>
     		"""% user_color)
