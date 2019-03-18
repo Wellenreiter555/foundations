cf666aebf56523cf06ea71ac458800cb6d454595
Max Egger <maximilian.egger@code.berlin>
2019-03-18 11:30:30 +0100

    foundations

--- treehouse/looping.py
+++
@@ -1,9 +0,0 @@
-name = input("What is your name? ")
-answer = input("Enter Yes or No? ")
-
-while answer.lower() != "yes":
-    print("That is totally fine, {}, while loops in Python repeat as long as a certain Boolean condition is met. Do you now understand while Loops in Python, {}? ".format(name, name))
-    answer = input("Enter Yes or No? ")
-else:
-    print("That is great, {}, keep going!".format(name))
-    
